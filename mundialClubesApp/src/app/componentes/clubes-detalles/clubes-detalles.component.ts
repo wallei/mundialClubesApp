ab1f5420761c419d11953516d561f8e854c305b4
Walter G. <wgazzanego@gmail.com>
2025-06-22 00:28:51 -0300

    Inicializamos componentes

---
+++ mundialClubesApp/src/app/componentes/clubes-detalles/clubes-detalles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clubes-detalles',
+  imports: [],
+  templateUrl: './clubes-detalles.component.html',
+  styleUrl: './clubes-detalles.component.css'
+})
+export class ClubesDetallesComponent {
+
+}
---
+++ mundialClubesApp/src/app/componentes/clubes/clubes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clubes',
+  imports: [],
+  templateUrl: './clubes.component.html',
+  styleUrl: './clubes.component.css'
+})
+export class ClubesComponent {
+
+}
