c3966209363231543e062c5151c166e7e803b662
leoqu1nt4na <leosquintana89@hotmail.com>
2025-06-23 20:30:17 -0300

    aguegando partidos

--- mundialClubesApp/src/app/componentes/partidos/partidos.component.ts
+++ mundialClubesApp/src/app/componentes/partidos/partidos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-partidos',
   imports: [],
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './partidos.component.css'
 })
 export class PartidosComponent {
+
+  horario1= '4:00 PM';
+  horario2= '10:00 PM';
 
 }
