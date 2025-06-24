f53e113ba94ee059d9616b742251020f9115c353
leoqu1nt4na <leosquintana89@hotmail.com>
2025-06-24 14:50:53 -0300

    formulario terminado

--- mundialClubesApp/src/app/componentes/contacto/contacto.component.ts
+++ mundialClubesApp/src/app/componentes/contacto/contacto.component.ts
@@ -1,11 +1,26 @@
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule,CommonModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
+
 export class ContactoComponent {
+
+  usuario = {
+    nombre:'',
+    email:'',
+    fanDe:'',
+  };
+
+  enviar() {
+    console.log('Formulario enviado:', this.usuario)
+  }
+
 
 }
